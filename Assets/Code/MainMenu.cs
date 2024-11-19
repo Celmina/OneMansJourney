a7ceb95937cab79ee7abe716d12e26e466159d82
Evita <evita.celmina@edu.rtu.lv>
2024-11-19 13:31:07 +0200

    PlayGame to 1st Island

--- Assets/Code/MainMenu.cs
+++ Assets/Code/MainMenu.cs
@@ -5,8 +5,10 @@ public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-        public void PlayGame() {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        public string sceneName;
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(sceneName);
         }
 
         public void QuitGame() {
