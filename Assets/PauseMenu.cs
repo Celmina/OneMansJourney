d243b4fff832d542ca485926e6ed08d0a0b76d37
Evita <evita.celmina@edu.rtu.lv>
2024-11-23 18:02:56 +0200

    Menu Controll

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,58 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public GameObject InventoryMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.visible = false; // Hide cursor on resume
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor back to the center of the screen
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true; // Make cursor visible
+        Cursor.lockState = CursorLockMode.None; // Free the cursor
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("SettingsMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game.....");
+        Application.Quit();
+    }
+
+
+}
