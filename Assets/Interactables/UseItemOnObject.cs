b0711efcb2afbee8883e3792ee5228f1ee695521
Linards657 <naglis.linards@gmail.com>
2024-12-09 17:37:43 +0200

    Uzlabots interact script + piemers ar koka tiltu

--- Assets/Interactables/UseItemOnObject.cs
+++ Assets/Interactables/UseItemOnObject.cs
@@ -4,16 +4,31 @@ public class UseItemOnObject : MonoBehaviour, IInteractable
 {
     private Inventory inventory;
     public GameObject itemToUse;
+    public bool consumeItem;
+    public bool destroyObject;
 
     void Start()
     {
         inventory = FindFirstObjectByType<Inventory>();
     }
+
     public void Interact()
+    {
+        if (consumeItem)
+        {
+            FConsumeItem();
+        }
+
+        if (destroyObject)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FConsumeItem()
     {
         if (inventory.checkInventory(itemToUse))
         {
-            Destroy(gameObject);
             inventory.removeItem(itemToUse);
         }
         else
@@ -22,3 +37,4 @@ public class UseItemOnObject : MonoBehaviour, IInteractable
         }
     }
 }
+
