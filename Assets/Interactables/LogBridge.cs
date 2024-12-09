0c3250f79ad8de7f36d6a5a06d15afd909a8cb1c
Linards657 <naglis.linards@gmail.com>
2024-12-09 17:40:20 +0200

    missing files

---
+++ Assets/Interactables/LogBridge.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+
+
+public class LogBridge : MonoBehaviour, IInteractable
+{
+    private Inventory inventory;
+    public GameObject itemToUse;
+    public GameObject wall;
+    public GameObject walking_log;
+
+    void Start()
+    {
+        inventory = FindFirstObjectByType<Inventory>();
+    }
+    public void Interact()
+    {
+        if (inventory.checkInventory(itemToUse))
+        {
+            walking_log.GetComponent<MeshRenderer>().enabled = true;
+            Destroy(gameObject);
+            Destroy(wall);
+        }
+    }
+}
