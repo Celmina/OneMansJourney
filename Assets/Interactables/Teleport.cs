7f16fc05f1e73396e90325dcd9a414a365590ec0
Evita <evita.celmina@edu.rtu.lv>
2024-12-12 18:12:05 +0200

    ToBeCoontinuedScene

--- Assets/Interactables/Teleport.cs
+++ Assets/Interactables/Teleport.cs
@@ -4,8 +4,47 @@ using UnityEngine.SceneManagement;
 public class Teleport : MonoBehaviour, IInteractable
 {
     public string sceneName;
+    public CanvasGroup fadeOverlay;
+    private bool isTransitioning = false;
+    private float transitionTime = 3f;
+    private float fadeTimer = 0f;
+
+    void Start()
+    {
+        if (fadeOverlay != null)
+        {
+            fadeOverlay.alpha = 0;
+            fadeOverlay.blocksRaycasts = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isTransitioning)
+        {
+            fadeTimer += Time.deltaTime;
+            fadeOverlay.alpha = Mathf.Clamp01(fadeTimer / transitionTime);
+            
+            if (fadeTimer >= transitionTime)
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+
     public void Interact()
     {
-        SceneManager.LoadScene(sceneName);
+        if (!isTransitioning)
+        {
+            StartTransition();
+        }
+    }
+
+    private void StartTransition()
+    {
+        isTransitioning = true;
+        fadeTimer = 0f;
+        fadeOverlay.alpha = 0f;
+        fadeOverlay.blocksRaycasts = true;
     }
 }
