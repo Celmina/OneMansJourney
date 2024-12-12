bbe97e843fc4a1c8d554846434444ff583bbf413
Evita <evita.celmina@edu.rtu.lv>
2024-12-12 14:44:10 +0200

    2ndIsland

--- Assets/TextOnHover.cs
+++ Assets/TextOnHover.cs
@@ -6,24 +6,38 @@ public class TextOnHover : MonoBehaviour
     public Transform InteractorSource;
     public float InteractRange;
     public TextMeshProUGUI hoverText;
+
     void Start()
     {
-        hoverText.gameObject.SetActive(false);
+        if (hoverText != null)
+        {
+            hoverText.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.LogError("Hover Text reference is missing!");
+        }
     }
 
     void Update()
     {
+        if (hoverText == null || InteractorSource == null) return;
+
         Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
         if (Physics.Raycast(r, out RaycastHit hitInfo, InteractRange))
         {
-            if (hitInfo.collider.gameObject.CompareTag("Hoverable"))
+            if (hitInfo.collider != null && hitInfo.collider.gameObject.CompareTag("Hoverable"))
             {
                 hoverText.gameObject.SetActive(true);
-                hoverText.transform.rotation = Quaternion.LookRotation(hoverText.transform.position - transform.position); //face camera
+                hoverText.transform.rotation = Quaternion.LookRotation(hoverText.transform.position - transform.position);
                 hoverText.transform.position = transform.position + transform.forward * 0.5f;
             }
+            else if (hoverText.gameObject.activeSelf)
+            {
+                hoverText.gameObject.SetActive(false);
+            }
         }
-        else
+        else if (hoverText.gameObject.activeSelf)
         {
             hoverText.gameObject.SetActive(false);
         }
